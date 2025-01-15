ec16ccbe88a42a8c976c8a0168424993c5e99026
Habibur Rahman <habibur88454@gmail.com>
2025-01-15 13:26:59 +0600

    setup server side

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Course plus is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Course plus is running! on port ${port}`)
+})
